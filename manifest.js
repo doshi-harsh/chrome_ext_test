c008e1dc790f933237bb0c2f12ea2b97beebe6fc
Harsh Doshi <h4doshi@gmail.com>
2019-05-29 15:28:25 -0400

    chrome extension testing code.

---
+++ manifest.js
@@ -0,0 +1,4 @@
+"background": {
+    "scripts": ["js/es6-promise.auto.min.js", "js/defaults.js", "js/speech.js", "js/document.js", "js/events.js", "js/stt.js", "js/listen.js"],
+    "persistent": false
+}
